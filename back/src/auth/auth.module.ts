0714d14ccb0ff0986029229440575b0c735d1683
SoyAndersonJoel <anderson.vilatuna@epn.edu.ec>
2026-04-13 01:56:22 -0500

    feat: Implementar servicio Auth

--- back/src/auth/auth.module.ts
+++ back/src/auth/auth.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Users } from '../user/entities/user.entity';
+import { UsersRepository } from '../user/user.repository';
+import { Role } from '../user/entities/roles.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Users, Role])],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, UsersRepository],
 })
 export class AuthModule {}
