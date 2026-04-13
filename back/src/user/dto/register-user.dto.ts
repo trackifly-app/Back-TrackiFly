8656319f029fdfb23b6cc31fab4824feae01ed3d
SoyAndersonJoel <anderson.vilatuna@epn.edu.ec>
2026-04-13 00:47:29 -0500

    feat: Integrar roleId

--- back/src/user/dto/register-user.dto.ts
+++ back/src/user/dto/register-user.dto.ts
@@ -4,7 +4,9 @@ import {
   IsEnum,
   IsNotEmpty,
   IsNumber,
+  IsOptional,
   IsString,
+  IsUUID,
   Max,
   MaxLength,
   Min,
@@ -18,7 +20,7 @@ export class RegisterUserDto {
   @MinLength(3, { message: 'El nombre debe tener almenos 3 caracteres' })
   @MaxLength(80, { message: 'E nombre no puede tener más de 80 caracteres' })
   name: string;
-  
+
   @IsNotEmpty({ message: 'El email no puede estar vacío' })
   @IsEmail({}, { message: 'El email debe tener una estructura válida' })
   email: string;
@@ -26,19 +28,23 @@ export class RegisterUserDto {
   @IsNotEmpty({ message: 'La contraseña no puede estar vacía' })
   @IsString({ message: 'La contraseña debe ser una cadena de texto' })
   @MinLength(6, { message: 'La contraseña debe tener al menos 6 caracteres' })
-  @MaxLength(100, { message: 'La contraseña no puede tener más de 100 caracteres' })
+  @MaxLength(100, {
+    message: 'La contraseña no puede tener más de 100 caracteres',
+  })
   password: string;
 
   @IsNotEmpty({ message: 'La dirección no puede estar vacía' })
   @IsString({ message: 'La dirección debe ser una cadena de texto' })
   @MinLength(3, { message: 'La dirección debe tener al menos 3 caracteres' })
-  @MaxLength(150, { message: 'La dirección no puede tener más de 150 caracteres' })
+  @MaxLength(150, {
+    message: 'La dirección no puede tener más de 150 caracteres',
+  })
   address: string;
 
   @IsNotEmpty({ message: 'El numero de telefono no puede estar vacío' })
   @IsNumber(
     { allowNaN: false, allowInfinity: false },
-    { message: 'Telefono debe ser un numero'}
+    { message: 'Telefono debe ser un numero' },
   )
   @Min(1000000, { message: 'El teléfono debe tener al menos 7 dígitos' })
   @Max(99999999999999, {
@@ -51,13 +57,18 @@ export class RegisterUserDto {
   gender: Gender;
 
   @IsNotEmpty({ message: 'La fecha de nacimiento no puede estar vacía' })
-  @IsDateString({}, { message: 'La fecha de nacimiento debe tener formato ISO' })
+  @IsDateString(
+    {},
+    { message: 'La fecha de nacimiento debe tener formato ISO' },
+  )
   birthdate: string;
-
   @IsNotEmpty({ message: 'El país no puede estar vacío' })
   @IsString({ message: 'El país debe ser una cadena de texto' })
   @MinLength(2, { message: 'El país debe tener al menos 2 caracteres' })
   @MaxLength(80, { message: 'El país no puede tener más de 80 caracteres' })
   country: string;
 
+  @IsOptional()
+  @IsUUID('4', { message: 'El rol debe ser un UUID válido' })
+  roleId: string;
 }
