d229f0978867d9c8fe04ef1728aadf5b85c1c52b
SoyAndersonJoel <anderson.vilatuna@epn.edu.ec>
2026-04-13 01:57:01 -0500

    feat: Implementar EndPoint de Auth

--- back/src/auth/auth.controller.ts
+++ back/src/auth/auth.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { RegisterUserDto } from '../user/dto/register-user.dto';
+import { LoginDto } from './dtos/login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  async singUp(@Body() newUserData: RegisterUserDto) {
+    return await this.authService.singUp(newUserData);
+  }
+
+  @Post('signin')
+  async singIn(@Body() credentials: LoginDto) {
+    const { email, password } = credentials;
+    return await this.authService.singIn(email, password);
+  }
 }
