1c2165731c921fd3194c2630946058a281c4f6f7
FlorCGutierrez <Florencia.carolina.gutierrez@gmail.com>
2026-04-16 00:24:37 -0300

    feat: enable CORS for all origins

--- back/src/main.ts
+++ back/src/main.ts
@@ -5,6 +5,11 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',       //permite conexion con front
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
