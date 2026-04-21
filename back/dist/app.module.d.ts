ea3c0099ef27c62e447fe6bfa50ae12994db4f3b
FlorCGutierrez <Florencia.carolina.gutierrez@gmail.com>
2026-04-20 23:37:29 -0300

    chore: remove dist from tracking

--- back/dist/app.module.d.ts
+++
@@ -1,8 +0,0 @@
-import { MiddlewareConsumer, NestModule, OnApplicationBootstrap } from "@nestjs/common";
-import { RolesService } from "./roles/roles.service";
-export declare class AppModule implements NestModule, OnApplicationBootstrap {
-    private readonly rolesService;
-    constructor(rolesService: RolesService);
-    configure(consumer: MiddlewareConsumer): void;
-    onApplicationBootstrap(): Promise<void>;
-}
