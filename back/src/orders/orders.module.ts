95c2eb6d72a9e205144940d2c4522cb4a62b2354
SoyAndersonJoel <anderson.vilatuna@epn.edu.ec>
2026-04-27 19:46:58 -0500

    refactor: Refactorizar nueva logica de pago de envios

--- back/src/orders/orders.module.ts
+++ back/src/orders/orders.module.ts
@@ -1,13 +1,17 @@
-import { Module } from "@nestjs/common";
+import { Module, forwardRef } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { OrdersController } from "./orders.controller";
 import { OrdersService } from "./orders.service";
 import { OrdersRepository } from "./orders.repository";
 import { Order } from "./entities/order.entity";
 import { OrderDetail } from "./entities/order-detail.entity";
+import { PaymentsModule } from "../payments/payments.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, OrderDetail])],
+  imports: [
+    TypeOrmModule.forFeature([Order, OrderDetail]),
+    forwardRef(() => PaymentsModule)
+  ],
   controllers: [OrdersController],
   providers: [OrdersService, OrdersRepository],
   exports: [OrdersRepository, OrdersService],
